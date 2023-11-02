3f0f7405f14cf14337f576fa321bb6c5344a379e
Endert Guerrero <alejandroguerrero200404@gmail.com>
2023-11-01 22:57:03 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ItemListContainer from './components/ItemListContainer'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
-export default Header;
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer titulo={"GTA V"} />
+    </>
+  )
+}
 
 export default App
